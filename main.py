dead644bf568d25e5154faaed5ab74f4082b3a61
Software Developer <134389179+dn0418@users.noreply.github.com>
2024-01-09 10:36:29 +0100

    Update password and url

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from openpyxl import Workbook, load_workbook
 
 
 UserName = 'nextaa'
-PassWord = 'Qwer1234'
+PassWord = 'Qwer4321'
 
 def check_file_writable(file_path):
     if not os.path.exists(file_path):
@@ -109,10 +109,10 @@ def ScrapeData():
     url = "https://www.m8clicks.com"
     early_url1 = "https://m8clicks.com/_View/RMOdds2.aspx?ot=e&ov=1&mt=0&wd="
     # early_url1 = "https://m8clicks.com/_View/RMOdds2.aspx?update=false&r=316466324&wd="
-    # "ot=e&ov=1&isWC=False&ia=0&isSiteFav=False"
-    early_url2 = "&isWC=False&ia=0&tf=-1"
-    today_url = "https://m8clicks.com/_View/RMOdds2.aspx?ot=t&ov=1&mt=0&wd=&isWC=False&ia=0&tf=-1&isSiteFav=False"
-    # "https://m8clicks.com/_View/RMOdds2.aspx?update=false&r=316466324&wd=2023-10-31&ot=e&isWC=False&ia=0&isSiteFav=False"
+    # "ot=e&ov=1&isWc=0&ia=0&isSiteFav=False"
+    early_url2 = "&isWc=0&ia=0&tf=-1"
+    today_url = "https://m8clicks.com/_View/RMOdds2.aspx?ot=t&ov=1&mt=0&wd=&isWc=0&ia=0&tf=-1&isSiteFav=False"
+    # "https://m8clicks.com/_View/RMOdds2.aspx?update=false&r=316466324&wd=2023-10-31&ot=e&isWc=0&ia=0&isSiteFav=False"
 
     chromeOptions = webdriver.ChromeOptions()
     chromeOptions.add_argument('--headless')
